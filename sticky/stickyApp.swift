286c730af5b21671d5f17431f58fba2f221e982e
FIGBERT <figbert@figbert.com>
2025-06-19 00:13:21 -0700

    Initial commit

---
+++ sticky/stickyApp.swift
@@ -0,0 +1,33 @@
+//
+//  stickyApp.swift
+//  sticky
+//
+//  Created by figbert on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct stickyApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
